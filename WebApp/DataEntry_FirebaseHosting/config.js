f41ddf8ee0d9ba60c3c2e54e4dffb7b4f02596a2
Shivam <theshivamlko@gmail.com>
2018-04-16 22:48:34 +0530

    webAppInProgress

---
+++ WebApp/DataEntry_FirebaseHosting/config.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCYT4zXh6bCkEAB03R4zyKuvgHekVyFhDo",
+    authDomain: "explore-india-8dc5b.firebaseapp.com",
+    databaseURL: "https://explore-india-8dc5b.firebaseio.com/",
+    storageBucket: "explore-india-8dc5b.appspot.com"
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
